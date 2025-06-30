10945ffa9d43985806c8d16040230deea037b5b3
Zheng Chen <94959679+Bellafc@users.noreply.github.com>
2025-06-30 16:00:07 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+from src.lm_polygraph.estimators import Eccentricity
+from src.lm_polygraph.utils.model import WhiteboxModel, BlackboxModel
+from src.lm_polygraph import estimate_uncertainty
+from src.lm_polygraph.estimators import MaximumTokenProbability, LexicalSimilarity, SemanticEntropy, PointwiseMutualInformation, EigValLaplacian, Focus,DegMat,KernelLanguageEntropy,LUQ, Verbalized1S
+import pandas as pd
+
+## put your openai api key inside the first ''
+## change the model you would like to use inside the second ''
+model = BlackboxModel('','gpt-3.5-turbo')
+eigv = EigValLaplacian(verbose=True)
+deg = DegMat(verbose=True)
+ecc = Eccentricity(verbose=True)
+lexsim = LexicalSimilarity()
+kle = KernelLanguageEntropy()
+luq = LUQ()
+
+##replace your data here
+df=pd.read_csv("gms8k.csv")
+for idx, row in df.iterrows():
+    if idx <200:
+        input=row['question']
+
+        input = f"""Raise three examples of code promples that are relavant to the initial prompt. Then solve the three relavant problem only.
+# Problem:
+{input}
+# Instructions:
+## Relevant Problems:
+- After "Q: ", describe the problem
+- After "A: ", explain the solution and enclose the ultimate answer here.e"""
+        estimator_list=[lexsim,kle]
+        for estimator in estimator_list:
+            thisue=estimate_uncertainty(model, estimator, input_text=input).uncertainty
+            print(idx,estimator,thisue)
+            df.at[idx, f"an PRR {estimator}"] = thisue
+    ##to save your data here
+    df.to_csv("gms8k.csv")
